06720b1d74e777f0ebf5e4ecf4bcb211cd198ff0
sang9268 <34136195+sang9268@users.noreply.github.com>
2025-05-11 03:46:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask, render_template  # ⭐ render_template 추가
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # ⭐ 반드시 템플릿 반환
+    return render_template('index.html')
+
+@app.route('/allview.html')  # 👈 이 부분 추가
+def allview():
+    return render_template('allview.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
